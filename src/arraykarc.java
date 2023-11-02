00facedec4129ea27174d5e5ff194845f6228893
student <student@vlab.local>
2023-11-02 12:58:36 +0100

    Initial commit

---
+++ src/arraykarc.java
@@ -0,0 +1,29 @@
+public class arraykarc {
+public static void main(String[] args){
+    int[] arraynumbers = new int[10];
+
+    for (int i = 0; i < arraynumbers.length; i++) {
+
+        arraynumbers[i] = 9 - i;
+    }
+
+    for (int num : arraynumbers) {
+        System.out.print(num + " ");
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+}
